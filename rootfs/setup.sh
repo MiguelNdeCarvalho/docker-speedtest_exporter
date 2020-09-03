af035bc4c9e05950011595415086c318ce1ec725
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-04 00:37:19 +0100

    rootfs: setup.sh: Make insecure connection for arm32 image

--- rootfs/setup.sh
+++ rootfs/setup.sh
@@ -2,8 +2,15 @@
 
 LAST_VERSION=`curl -s https://api.github.com/repos/nlamirault/speedtest_exporter/releases/latest | grep -Po '"tag_name": "\K.*?(?=")'`
 LAST_VERSION="${LAST_VERSION:1}"
+ARCH=`uname -m`
 
 mkdir /app
 cd /app
-wget -q -O speedtest_exporter https://bintray.com/artifact/download/nlamirault/oss/speedtest_exporter-${LAST_VERSION}_linux_${ARCH}
+
+if [ $ARCH == "armv7l"];then
+	wget -q --no-check-certificate -O speedtest_exporter https://bintray.com/artifact/download/nlamirault/oss/speedtest_exporter-${LAST_VERSION}_linux_${ARCH} 
+else
+	wget -q -O speedtest_exporter https://bintray.com/artifact/download/nlamirault/oss/speedtest_exporter-${LAST_VERSION}_linux_${ARCH}
+fi
+
 chmod +x /app/speedtest_exporter
