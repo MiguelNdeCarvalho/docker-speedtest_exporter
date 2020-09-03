e75499aa2c957577cad5ef68ee44efd1380b4491
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-03 19:56:18 +0100

    Everything almost Done Arch still not working

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+ARG ARCH=
+FROM $ARCH/alpine:3.12
+
+LABEL maintainer="MiguelNdeCarvalho <geral@miguelndecarvalho.pt>"
+
+RUN apk upgrade --no-cache \
+ && apk add --no-cache \
+ 	curl \
+	grep
+
+COPY rootfs .
+
+RUN /setup.sh
+
+EXPOSE 9112
+
+CMD ["/app/speedtest_exporter"]
+
+HEALTHCHECK --timeout=10s CMD curl --silent --fail http://127.0.0.1:9112/metrics
---
+++ rootfs/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+LAST_VERSION=`curl --silent "https://api.github.com/repos/nlamirault/speedtest_exporter/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+DOWNLOAD_URL=`https://bintray.com/artifact/download/nlamirault/oss/speedtest_exporter-${LAST_VERSION}_linux_${ARCH}`
+
+
+
+mkdir /app
+wget -qO /app/node_exporter $DOWNLOAD_URL
+chmod +x /app/node_exporter
